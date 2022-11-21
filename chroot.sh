96c81217f458a6e15e1a1980efcb9c03e40ae756
tsvetkovivan1993 <39021508+tsvetkovivan1993@users.noreply.github.com>
2022-11-21 17:14:44 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -4,5 +4,4 @@ grub2-mkconfig -o /boot/grub2/grub.cfg
 cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed -i "s/initramfs-//g;s/.img//g"` --force; done
 sed -i 's|rd.lvm.lv=VolGroup00/LogVol00|rd.lvm.lv=vg_root/lv_root|g' /boot/grub2/grub.cfg
 exit
-reboot
 EOF
