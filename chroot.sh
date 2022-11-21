120fda7d93386d6ee0133a1f372cb0b6d8b6b354
tsvetkovivan1993 <39021508+tsvetkovivan1993@users.noreply.github.com>
2022-11-21 14:29:36 +0800

    Create chroot.sh

---
+++ chroot.sh
@@ -0,0 +1,5 @@
+chroot /mnt/
+grub2-mkconfig -o /boot/grub2/grub.cfg
+cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g;s/.img//g"` --force; done
+echo "sed -i 's|rd.lvm.lv=VolGroup00/LogVol00|rd.lvm.lv=vg_root/lv_root|g' /boot/grub2/grub.cfg"
+exit
