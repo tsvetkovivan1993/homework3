875b2cbdc28b78c28b32646fa0a2794473a39e4e
tsvetkovivan1993 <39021508+tsvetkovivan1993@users.noreply.github.com>
2022-11-21 16:27:11 +0800

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 cat << EOF | chroot /mnt/ /bin/bash
 grub2-mkconfig -o /boot/grub2/grub.cfg
 cd /boot ; for i in `ls initramfs-*img`; do dracut -v $i `echo $i|sed "s/initramfs-//g;s/.img//g"` --force; done
